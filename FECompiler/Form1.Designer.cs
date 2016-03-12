2ad303b46c4bb0cad0cc1ac71e37a9b081a66cb8
Edwin Hidalgo Lopez <edwin_hidalgo1@ucne.edu.do>
2016-03-12 12:04:30 -0400

    Subiendo todo

---
+++ FECompiler/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace FECompiler
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.strarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cerrarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.formatoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.CompiladorRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.MaincolorDialog = new System.Windows.Forms.ColorDialog();
+            this.MainfontDialog = new System.Windows.Forms.FontDialog();
+            this.MainopenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.MainsaveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.strarToolStripMenuItem,
+            this.archivoToolStripMenuItem,
+            this.colorToolStripMenuItem,
+            this.formatoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(919, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // strarToolStripMenuItem
+            // 
+            this.strarToolStripMenuItem.Image = global::FECompiler.Properties.Resources._1457574393_Play;
+            this.strarToolStripMenuItem.Name = "strarToolStripMenuItem";
+            this.strarToolStripMenuItem.Size = new System.Drawing.Size(28, 20);
+            this.strarToolStripMenuItem.Click += new System.EventHandler(this.strarToolStripMenuItem_Click);
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirToolStripMenuItem,
+            this.guardarToolStripMenuItem,
+            this.cerrarToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.abrirToolStripMenuItem.Text = "Abrir";
+            this.abrirToolStripMenuItem.Click += new System.EventHandler(this.abrirToolStripMenuItem_Click);
+            // 
+            // guardarToolStripMenuItem
+            // 
+            this.guardarToolStripMenuItem.Name = "guardarToolStripMenuItem";
+            this.guardarToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.guardarToolStripMenuItem.Text = "Guardar";
+            this.guardarToolStripMenuItem.Click += new System.EventHandler(this.guardarToolStripMenuItem_Click);
+            // 
+            // cerrarToolStripMenuItem
+            // 
+            this.cerrarToolStripMenuItem.Name = "cerrarToolStripMenuItem";
+            this.cerrarToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.cerrarToolStripMenuItem.Text = "Cerrar";
+            this.cerrarToolStripMenuItem.Click += new System.EventHandler(this.cerrarToolStripMenuItem_Click);
+            // 
+            // colorToolStripMenuItem
+            // 
+            this.colorToolStripMenuItem.Name = "colorToolStripMenuItem";
+            this.colorToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.colorToolStripMenuItem.Text = "Color";
+            this.colorToolStripMenuItem.Click += new System.EventHandler(this.colorToolStripMenuItem_Click);
+            // 
+            // formatoToolStripMenuItem
+            // 
+            this.formatoToolStripMenuItem.Name = "formatoToolStripMenuItem";
+            this.formatoToolStripMenuItem.Size = new System.Drawing.Size(64, 20);
+            this.formatoToolStripMenuItem.Text = "Formato";
+            this.formatoToolStripMenuItem.Click += new System.EventHandler(this.formatoToolStripMenuItem_Click);
+            // 
+            // CompiladortextBox
+            // 
+            this.CompiladorRichTextBox.Location = new System.Drawing.Point(0, 28);
+            this.CompiladorRichTextBox.Name = "CompiladortextBox";
+            this.CompiladorRichTextBox.Size = new System.Drawing.Size(919, 466);
+            this.CompiladorRichTextBox.TabIndex = 1;
+            this.CompiladorRichTextBox.Text = "";
+            // 
+            // MainopenFileDialog
+            // 
+            this.MainopenFileDialog.FileName = "openFileDialog1";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(919, 493);
+            this.Controls.Add(this.CompiladorRichTextBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "FECompiler 0.1.3";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.RichTextBox CompiladorRichTextBox;
+        private System.Windows.Forms.ToolStripMenuItem strarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem guardarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cerrarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem colorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem formatoToolStripMenuItem;
+        private System.Windows.Forms.ColorDialog MaincolorDialog;
+        private System.Windows.Forms.FontDialog MainfontDialog;
+        private System.Windows.Forms.OpenFileDialog MainopenFileDialog;
+        private System.Windows.Forms.SaveFileDialog MainsaveFileDialog;
+    }
+}
+
---
+++ FECompiler/Form1.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace FECompiler
+{
+    public partial class MainForm : Form
+    {
+        [System.Runtime.InteropServices.DllImport("Kernel32")]
+        public static extern bool AllocConsole();
+        [System.Runtime.InteropServices.DllImport("Kernel32")]
+        public static extern bool FreeConsole();
+
+        //Funciones a utilizar
+        const string Multiplicar = "#mul";
+        const string Dividir = "#div";
+        const string Sumar = "#sum";
+        const string Restar = "#res";
+        const string Porcentaje = "#por";
+        const string RaizCuadrada = "#rac";
+        const string Concatenar = "#con";
+        const string Imprimir = "#imp";
+        string Codigo;
+        List<string> Var = new List<string>();
+        List<string> Val = new List<string>();
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        public bool Variables()
+        {
+            bool retorno = false;
+            int menor = 0;
+            int mayor = 0;
+            int indice = 0;
+            string aux, aux2; ;
+            string variables;
+            menor = Codigo.IndexOf('<');
+            mayor = Codigo.IndexOf('>');
+            if (menor != -1 || mayor != -1)
+            {
+                variables = Codigo.Substring(menor + 1, mayor - 1);
+
+                //agregando variables
+                do
+                {
+                    indice = variables.IndexOf(";");
+                    aux = variables.Substring(0, indice);
+                    variables = variables.Substring(indice);
+                    menor = aux.IndexOf('=');
+                    mayor = aux.Length;
+                    aux2 = aux.Substring(menor + 1);
+                    menor -= 4;
+                    aux = aux.Substring(4, menor);
+                    Var.Add(aux);
+                    Val.Add(aux2);
+                    retorno = true;
+                } while (variables.Length != variables.Length);
+            }
+            return retorno;
+        }
+
+        private void abrirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MainopenFileDialog.ShowDialog();
+            StreamReader file = new StreamReader(MainopenFileDialog.FileName);
+            CompiladorRichTextBox.Text = file.ReadLine();            
+        }
+
+        private void guardarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MainsaveFileDialog.FileName = "New File.fe";
+            var sf = MainsaveFileDialog.ShowDialog();
+            if (sf == DialogResult.OK)
+            {
+                using (var savefile = new StreamWriter(MainsaveFileDialog.FileName))
+                {
+                    savefile.WriteLine(CompiladorRichTextBox);
+                }                    
+            }
+        }
+
+        private void cerrarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void colorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var cl = MaincolorDialog.ShowDialog();
+            if (cl == DialogResult.OK)
+            {
+                CompiladorRichTextBox.ForeColor = MaincolorDialog.Color;
+            }
+        }
+
+        private void formatoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var fm = MainfontDialog.ShowDialog();
+            if (fm == DialogResult.OK)
+            {
+                CompiladorRichTextBox.Font = MainfontDialog.Font;
+            }
+        }
+
+        public void Ejecutar(string resultado)
+        {
+            Thread.Sleep(2000);
+            AllocConsole();
+            Console.Title = "FECompiler 0.1.3";
+            Console.WriteLine("El resultado es: "+resultado);
+            Console.Write("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+            Console.Write("------------------------------------------------------------\n");
+            Console.Write("por favor precione una tecle para continuar...");
+            Console.ReadKey();
+            FreeConsole();
+        }
+
+        private void strarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Compilar();
+        }
+
+        void Compilar()
+        {
+            bool varia = false;
+            Codigo = CompiladorRichTextBox.Text;
+            int menor;
+            int mayor;
+            menor = Codigo.IndexOf('<');
+            mayor = Codigo.IndexOf('>');
+            if (menor + 1 != mayor)
+                varia = Variables();
+
+            Codigo = Codigo.Substring(Codigo.IndexOf("#"));
+            string Funcion = Codigo.Substring(0, 4);
+            string Ejecucion = Codigo.Substring(4);
+
+            if (Funcion == Sumar)
+            {
+                int Ap = Ejecucion.IndexOf("(");
+                int CP = Ejecucion.IndexOf(")");
+                int C = Ejecucion.IndexOf(",");
+                double Num, Num1;
+                int num1 = C;
+                int num2 = CP - C;
+                if (varia == true)
+                {
+
+                    Ejecucion = Ejecucion.Replace(Var[0].ToString(), Val[0].ToString());
+                    Var = new List<string>();
+                    Val = new List<string>();
+                    Ap = Ejecucion.IndexOf("(");
+                    CP = Ejecucion.IndexOf(")");
+                    C = Ejecucion.IndexOf(",");
+                    num1 = C;
+                    num2 = CP - C;
+                    double.TryParse((Ejecucion.Substring(1, (num1 - 1))), out Num);
+                    double.TryParse((Ejecucion.Substring((C + 1), (num2 - 1))), out Num1);
+                    Ejecutar((Num + Num1).ToString());
+                    //CompiladorRichTextBox.Text += (Num + Num1).ToString();
+                }
+                else
+                {
+                    double.TryParse((Ejecucion.Substring(1, (num1 - 1))), out Num);
+                    double.TryParse((Ejecucion.Substring((C + 1), (num2 - 1))), out Num1);
+                    Ejecutar((Num + Num1).ToString());
+                    //CompiladorRichTextBox.Text += (Num + Num1).ToString();
+                }
+            }
+            else if (Funcion == Restar)
+            {
+                int Ap = Ejecucion.IndexOf("(");
+                int CP = Ejecucion.IndexOf(")");
+                int C = Ejecucion.IndexOf(",");
+                double Num, Num1;
+                int num1 = C;
+                int num2 = CP - C;
+                if (varia == true)
+                {
+
+                    Ejecucion = Ejecucion.Replace(Var[0].ToString(), Val[0].ToString());
+                    Var = new List<string>();
+                    Val = new List<string>();
+                    Ap = Ejecucion.IndexOf("(");
+                    CP = Ejecucion.IndexOf(")");
+                    C = Ejecucion.IndexOf(",");
+                    num1 = C;
+                    num2 = CP - C;
+                    double.TryParse((Ejecucion.Substring(1, (num1 - 1))), out Num);
+                    double.TryParse((Ejecucion.Substring((C + 1), (num2 - 1))), out Num1);
+                    Ejecutar((Num - Num1).ToString());
+                    //CompiladorRichTextBox.Text += (Num - Num1).ToString();
+                }
+                else
+                {
+                    double.TryParse((Ejecucion.Substring(1, (num1 - 1))), out Num);
+                    double.TryParse((Ejecucion.Substring((C + 1), (num2 - 1))), out Num1);
+                    Ejecutar((Num - Num1).ToString());
+                    //CompiladorRichTextBox.Text += (Num - Num1).ToString();
+                }
+            }
+            else if (Funcion == Multiplicar)
+            {
+                int Ap = Ejecucion.IndexOf("(");
+                int CP = Ejecucion.IndexOf(")");
+                int C = Ejecucion.IndexOf(",");
+                double Num, Num1;
+                int num1 = C;
+                int num2 = CP - C;
+                if (varia == true)
+                {
+
+                    Ejecucion = Ejecucion.Replace(Var[0].ToString(), Val[0].ToString());
+                    Var = new List<string>();
+                    Val = new List<string>();
+                    Ap = Ejecucion.IndexOf("(");
+                    CP = Ejecucion.IndexOf(")");
+                    C = Ejecucion.IndexOf(",");
+                    num1 = C;
+                    num2 = CP - C;
+                    double.TryParse((Ejecucion.Substring(1, (num1 - 1))), out Num);
+                    double.TryParse((Ejecucion.Substring((C + 1), (num2 - 1))), out Num1);
+                    Ejecutar((Num * Num1).ToString());
+                    //CompiladorRichTextBox.Text += (Num * Num1).ToString();
+                }
+                else
+                {
+                    double.TryParse((Ejecucion.Substring(1, (num1 - 1))), out Num);
+                    double.TryParse((Ejecucion.Substring((C + 1), (num2 - 1))), out Num1);
+                    Ejecutar((Num * Num1).ToString());
+                    //CompiladorRichTextBox.Text += (Num * Num1).ToString();
+                }
+            }
+            else if (Funcion == Dividir)
+            {
+                int Ap = Ejecucion.IndexOf("(");
+                int CP = Ejecucion.IndexOf(")");
+                int C = Ejecucion.IndexOf(",");
+                double Num, Num1;
+                int num1 = C;
+                int num2 = CP - C;
+                if (varia == true)
+                {
+
+                    Ejecucion = Ejecucion.Replace(Var[0].ToString(), Val[0].ToString());
+                    Var = new List<string>();
+                    Val = new List<string>();
+                    Ap = Ejecucion.IndexOf("(");
+                    CP = Ejecucion.IndexOf(")");
+                    C = Ejecucion.IndexOf(",");
+                    num1 = C;
+                    num2 = CP - C;
+                    double.TryParse((Ejecucion.Substring(1, (num1 - 1))), out Num);
+                    double.TryParse((Ejecucion.Substring((C + 1), (num2 - 1))), out Num1);
+                    Ejecutar((Num / Num1).ToString());
+                    //CompiladorRichTextBox.Text += (Num / Num1).ToString();
+                }
+                else
+                {
+                    double.TryParse((Ejecucion.Substring(1, (num1 - 1))), out Num);
+                    double.TryParse((Ejecucion.Substring((C + 1), (num2 - 1))), out Num1);
+                    Ejecutar((Num / Num1).ToString());
+                    //CompiladorRichTextBox.Text += (Num / Num1).ToString();
+                }
+            }
+            else if (Funcion == Porcentaje)
+            {
+                int Ap = Ejecucion.IndexOf("(");
+                int CP = Ejecucion.IndexOf(")");
+                int C = Ejecucion.IndexOf(",");
+                double Num;
+                int num1 = C;
+                int num2 = CP - C;
+                double.TryParse((Ejecucion.Substring(1, (num1 - 1))), out Num);
+                int.TryParse((Ejecucion.Substring((C + 1), (num2 - 1))), out num2);
+                Ejecutar(((Num * num2) / 100).ToString());
+                //CompiladorRichTextBox.Text += ((Num * num2) / 100).ToString();
+            }
+            else if (Funcion == RaizCuadrada)
+            {
+                int Ap = Ejecucion.IndexOf("(");
+                int CP = Ejecucion.IndexOf(")");
+                double Num;
+                int num1 = CP;
+                double.TryParse((Ejecucion.Substring(1, (num1 - 1))), out Num);
+                Ejecutar((Math.Sqrt(Num)).ToString());
+                //CompiladorRichTextBox.Text += (Math.Sqrt(Num)).ToString();
+            }
+            else if (Funcion == Concatenar)
+            {
+                int Ap = Ejecucion.IndexOf("(");
+                int CP = Ejecucion.IndexOf(")");
+                int C = Ejecucion.IndexOf(",");
+                int num1 = C;
+                int num2 = CP - C;
+                string s1 = Ejecucion.Substring(1, (num1 - 1));
+                string s2 = Ejecucion.Substring((C + 1), (num2 - 1));
+                Ejecutar(string.Concat(s1, s2));
+                //CompiladorRichTextBox.Text += string.Concat(s1, s2);
+            }
+            else if (Funcion == Imprimir)
+            {
+                int Ap = Ejecucion.IndexOf("(");
+                int CP = Ejecucion.IndexOf(")");
+                Ejecutar(Ejecucion.Substring(1, (CP - 1)));
+                //CompiladorRichTextBox.Text += Ejecucion.Substring(1, (CP - 1));
+            }
+            else
+            {
+                MessageBox.Show("El formato no es Correcto!! \n Verifique el codigo.");
+            }
+
+        }
+    }
+}
